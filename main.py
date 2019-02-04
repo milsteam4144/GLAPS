ec445e69b2c7f32844485dd036ea4eab914b947e
Gabriela Canjura <canjurag4010@ad.faytechcc.edu>
2019-02-04 16:53:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -89,10 +89,6 @@ years = [2011, 2012, 2013, 2014, 2015, 2016, 2017]
 
 #______________________________ LOOK HERE
 
-atList = []
-atList1 = []
-atList2 = []
-
 censusList = []
 
 for row1 in session.query(censusTables).all():
