9c4c2429f4f4946c5bef670176b4952e8a9c4913
gmastorg <31484932+gmastorg@users.noreply.github.com>
2019-02-11 14:55:34 -0500

    added non stadium states

--- apis.py
+++ apis.py
@@ -30,15 +30,15 @@ def getCountyCode():
     session = Session()
 
 
-    #Upload the ZipCodes table as a Table object
-    zipCodes = Table('ZipCodes', metadata, autoload = True, autoload_with=engine)
+    #Upload the stadiums table as a Table object
+    stadiums = Table('Stadiums', metadata, autoload = True, autoload_with=engine)
 
 
     url = requests.get("https://api.census.gov/data/2011/acs/acs1?get=NAME,B01001_001E&for=county:*&in=state:*&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
     #response = url.read().decode('utf-8')
     responseJson = list(json.loads(url.text))
     
-    for row in session.query(zipCodes).all():  
+    for row in session.query(stadiums).all():  
         for item in list(responseJson):
             if item[0] == str(row.County) + " County, " + str(row.State):
                 stadiumCountyStateCodes[item[2]]=item[3]
@@ -57,12 +57,21 @@ def countyCodesRandom():
     while i<=56:
         if i == 3 or i == 7 or i == 14 or i == 43 or i == 52:
             i += 1
-        states.append(i)
-        i +=1    
+        if i < 10:
+            i = '0'+str(i)
+        states.append(str(i))
+        i = int(i) +1  
 
     for state in states:
         x = random.randint(1,300)
         
+        if x < 100:
+            if x < 10:
+                x = "00"+str(x)
+            else:
+                x = "0"+str(x)
+        else:       
+            x = str(x)
         counties.append(x)  
     
     countyStateDict = dict(zip(states,counties))
@@ -73,60 +82,46 @@ def countyCodesRandom():
                 countyStateDict[key] = random.randint(1,300)
     
     return countyStateDict
-    #print (countyStateDict)
-"""    
-def getStadiumCensusData(year,countyName,state,census_table):
-    countyInfo = getCountyCode(countyName, state)
+    #print (countyStateDict) 
+
+def getAllCounties():
+    
+    allCounties = {}
+    
+    dicts0 = getCountyCode()
+    dicts1 = countyCodesRandom()
+    dicts2 = countyCodesRandom()
+   
+    allCounties.update(dicts0)
+    allCounties.update(dicts1)
+    allCounties.update(dicts2)
+        
+    return allCounties
+    
+def getCensusData(year, county, state, census_table):    
+
     url = ''    
-    state = countyInfo[1]
-    county = countyInfo[0]
-    
-    
     if census_table.startswith('S'):
-        url = requests.get("https://api.census.gov/data/"+str(year)+"/acs/acs1/subject?get="+census_table+",NAME&for=county:"+county+"&in=state:"+state+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
+        url = requests.get("https://api.census.gov/data/"+str(year)+"/acs/acs1/subject?get="+census_table+",NAME&for=county:"+str(county)+"&in=state:"+str(state)+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
         responseJson = list(json.loads(url.text))
         if year == 2017:
             return responseJson [1][0], responseJson[1][1], responseJson[1][2], responseJson[1][3]
-        else:
+        elif year < 2017:
             return responseJson [1][0], responseJson[1][1], responseJson[1][2], responseJson[1][4]
+        else:
+            return 0
     if census_table.startswith('D'):
-        url = requests.get("https://api.census.gov/data/" + str(year) + "/acs/acs5/profile?get="+census_table+",NAME&for=county:"+county+"&in=state:"+state+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
+        url = requests.get("https://api.census.gov/data/" + str(year) + "/acs/acs5/profile?get="+census_table+",NAME&for=county:"+str(county)+"&in=state:"+str(state)+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
         responseJson = list(json.loads(url.text))
         return responseJson [1][0], responseJson[1][2], responseJson[1][2],responseJson[1][3], responseJson [1][4]
+    else:
+            return 0
     if census_table.startswith('B'):
-        url = requests.get("https://api.census.gov/data/"+str(year)+"/acs/acs1/?get="+census_table+",NAME&for=county:"+county+"&in=state:"+state+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
+        url = requests.get("https://api.census.gov/data/"+str(year)+"/acs/acs1/?get="+census_table+",NAME&for=county:"+str(county)+"&in=state:"+str(state)+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
         responseJson = list(json.loads(url.text))
         return responseJson [1][0], responseJson[1][1], responseJson[1][2],responseJson[1][3],responseJson[1][4]
     else:
         return 0
-"""
-    
-def getCensusData(year, census_table, countiesDict):    
-    
-    allCounties  = {}
-    allCounties.append(getCountyCode())
-    allCounties.append(countyCodesRandom())
-    allCounties.append(countyCodesRandom())
-    url = ''    
-  
-    for key in allCounties:
-        if census_table.startswith('S'):
-            url = requests.get("https://api.census.gov/data/"+str(year)+"/acs/acs1/subject?get="+census_table+",NAME&for=county:"+allCounties[key]+"&in=state:"+key+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
-            responseJson = list(json.loads(url.text))
-            if year == 2017:
-                return responseJson [1][0], responseJson[1][1], responseJson[1][2], responseJson[1][3]
-            else:
-                return allCounties[key], key, responseJson [1][0], responseJson[1][1], responseJson[1][2], responseJson[1][4]
-        if census_table.startswith('D'):
-            url = requests.get("https://api.census.gov/data/" + str(year) + "/acs/acs5/profile?get="+census_table+",NAME&for=county:"+allCounties[key]+"&in=state:"+key+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
-            responseJson = list(json.loads(url.text))
-            return allCounties[key], key, responseJson [1][0], responseJson[1][2], responseJson[1][2],responseJson[1][3], responseJson [1][4]
-        if census_table.startswith('B'):
-            url = requests.get("https://api.census.gov/data/"+str(year)+"/acs/acs1/?get="+census_table+",NAME&for=county:"+allCounties[key]+"&in=state:"+key+"&key=02a32d03b6dff733b0973d974df5e01c2de1daf3")
-            responseJson = list(json.loads(url.text))
-            return allCounties[key], key, responseJson [1][0], responseJson[1][1], responseJson[1][2],responseJson[1][3],responseJson[1][4]
-        else:
-            return 0
 
 #cumbPop = getPop(2011, "Cumberland", "North Carolina")
 #print(cumbPop)
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 13:44:15 2019
+
+@author: canjurag4010
+"""
+
 import json
 from urllib.request import urlopen
 from sqlalchemy import create_engine
@@ -10,7 +17,7 @@ from sqlalchemy import Index
 from sqlalchemy.orm import relationship, backref
 import logging
 from sqlalchemy.orm import sessionmaker
-from apis import getCensusData
+from apis import getCountyCode, getCensusData, getAllCounties
 
 
 path = os.path.abspath("MinorLeague.db")
@@ -27,29 +34,27 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-#Upload the ZipCodes table as a Table object
-zipCodes = Table('ZipCodes', metadata, autoload = True, autoload_with=engine)
+#Upload the Stadiums table as a Table object
+stadiums = Table('Stadiums', metadata, autoload = True, autoload_with=engine)
 censusTables = Table('CensusTables',metadata, autoload = True, autoload_with=engine)
-#print(zipCodes.columns)
+#print(stadiums.columns)
 
 #The following are three tables with various attributes (columns)
 
 class Subject_T(Base):
     
     __tablename__ = "Subject"
-    year = Column('year', Integer, primary_key = True)
-    stadiumName = Column('stadium', String(40), primary_key = True)
-    stadiumID = Column(Integer, ForeignKey("ZipCodes.ID"), nullable=False)
+    year = Column('year', Integer, primary_key = True, nullable=False)
+    locationID = Column(Integer, primary_key = True, nullable=False)
     meanIncome = Column('meanIncome', Integer)
     medianIncome = Column('medianIncome', Integer)
     povertyPop = Column('povertyPop', Integer)
     medianAge = Column('medianAge', Integer)
     
-    def __init__(self, year, stadiumName, stadiumID, meanIncome,medianIncome,povertyPop,medianAge):
+    def __init__(self, year, locationID, meanIncome,medianIncome,povertyPop,medianAge):
         
         self.year = year
-        self.stadiumName = stadiumName
-        self.stadiumID = stadiumID
+        self.locationID = locationID
         self.meanIncome = meanIncome
         self.medianIncome = medianIncome
         self.povertyPop = povertyPop
@@ -59,9 +64,8 @@ class Subject_T(Base):
 class Data_Profile_T(Base):
     
     __tablename__ = "Data_Profile"
-    year = Column('year', Integer, primary_key = True)
-    stadiumName = Column('stadium', String(40), primary_key = True)
-    stadiumID = Column(Integer, ForeignKey("ZipCodes.ID"), nullable=False)
+    year = Column('year', Integer, primary_key = True, nullable=False)
+    locationID = Column(Integer, primary_key = True, nullable=False)
     workers = Column('workers', Integer)
     medianHouseIncome = Column('medianHouseIncome', Integer)
     medianFamilyIncome = Column('medianFamilyIncome', Integer)
@@ -69,12 +73,11 @@ class Data_Profile_T(Base):
     medianWorkerIncome = Column('medianWorkerIncome', Integer)
     
     
-    def __init__(self, year, stadiumName, stadiumID, \
+    def __init__(self, year, locationID, \
                  workers, medianHouseIncome, medianFamilyIncome, medianNonFamIncome, medianWorkerIncome):
         
         self.year = year
-        self.stadiumName = stadiumName
-        self.stadiumID = stadiumID
+        self.locationID = locationID
         self.workers = workers
         self.medianHouseIncome = medianHouseIncome
         self.medianFamilyIncome = medianFamilyIncome
@@ -84,9 +87,8 @@ class Data_Profile_T(Base):
 class Detailed_T(Base):
     
     __tablename__ = "Detailed"
-    year = Column('year', Integer, primary_key = True)
-    stadiumName = Column('stadium', String(40), primary_key = True)
-    stadiumID = Column(Integer, ForeignKey("ZipCodes.ID"), nullable=False)
+    year = Column('year', Integer, primary_key = True, nullable=False)
+    locationID = Column(Integer, primary_key = True, nullable=False)
     population = Column('population', Integer)
     medianRealEstateTax = Column('medianRealEstateTax', Integer)
     medianHouseholdCosts = Column('medianHouseholdCosts', Integer)
@@ -95,50 +97,69 @@ class Detailed_T(Base):
 
     
     
-    def __init__(self, year, stadiumName, stadiumID, population\
+    def __init__(self, year, locationID, population\
                  ,medianRealEstateTax,medianHouseholdCosts,totalHouses,medianHomeVal):
         
         self.year = year
-        self.stadiumName = stadiumName
-        self.stadiumID = stadiumID
+        self.locationID = locationID
         self.population = population
         self.medianRealEstateTax = medianRealEstateTax
         self.medianHouseholdCosts = medianHouseholdCosts
         self.totalHouses = totalHouses
         self.medianHomeVal = medianHomeVal
+        
+class Locations(Base):
+    
+    __tablename__ = "Locations"
+    __table_args__ = {'sqlite_autoincrement':True}
+    locationID = Column(Integer, primary_key = True)
+    countyCode = Column('CountyCode', String)
+    stateCode = Column('StateCode', String)
+    stadiumExists = Column('StadiumExists', Integer)
+    
+    def __init__(self, locationID, countyCode, stateCode, stadiumExists):
+        
+        self.locationID = locationID
+        self.countyCode = countyCode
+        self.stateCode = stateCode
+        self.stadiumExists = stadiumExists
        
 # Create the tables
 Base.metadata.create_all(engine)
-    
-"""  
-jimmystadium = session.query(zipCodes).filter_by(ZipCode = 48317).one() #This returns the item from the table as an object
-print(jimmystadium.City) #Then you can access it's attributes directly
-"""
        
-
 # A list of years that we need data for
 years = [2011, 2012, 2013, 2014, 2015, 2016, 2017]
 
 censusList = []
-
+x = 0
 for row1 in session.query(censusTables).all():
     censusList.append(row1.TableNames)
     
 #print(censusList[0]+"Next:"+ censusList[1]+"Next:"+ censusList[2])
 
+allCounties = getAllCounties()
     
-for year in years:
-    county, state, meanIncome, medianIncome, povertyPop, medianAge = getCensusData(year,censusList[0])
-    new = Subject_T(year, county, state, meanIncome, medianIncome, povertyPop, medianAge)
+for key in allCounties: 
+    x +=1
+    new = Locations(locationID = x, countyCode = allCounties[key], stateCode = key, stadiumExists = 0)
     session.add(new)
+
+session.commit()
+session.flush()
+  
+for row in session.query(Locations).all():
+    for year in years:
+        meanIncome, medianIncome, povertyPop, medianAge = getCensusData(year, row.countyCode, row.stateCode, censusList[0])
+        new = Subject_T(year, row.locationID,  meanIncome, medianIncome, povertyPop, medianAge)
+        session.add(new)
     
-    county, state, workers,medianHouseIncome,medianFamilyIncome,medianNonFamIncome,medianWorkerIncome = getCensusData(year, censusList[1])
-    new = Data_Profile_T(year, county, state, workers, medianHouseIncome, medianFamilyIncome, medianNonFamIncome, medianWorkerIncome)
-    session.add(new)
-    
-    county, state, population,medianRealEstateTax,medianHouseholdCosts,totalHouses,medianHomeVal = getCensusData(year, censusList[2])
-    new = Detailed_T(year, county, state, population, medianRealEstateTax,medianHouseholdCosts,totalHouses,medianHomeVal)
-    session.add(new)
+        workers,medianHouseIncome,medianFamilyIncome,medianNonFamIncome,medianWorkerIncome = getCensusData(year, row.countyCode, row.stateCode, censusList[1])
+        new = Data_Profile_T(year, row.locationID, workers, medianHouseIncome, medianFamilyIncome, medianNonFamIncome, medianWorkerIncome)
+        session.add(new)
+        
+        population,medianRealEstateTax,medianHouseholdCosts,totalHouses,medianHomeVal = getCensusData(year, row.countyCode, row.stateCode, censusList[2])
+        new = Detailed_T(year, row.locationID, population, medianRealEstateTax,medianHouseholdCosts,totalHouses,medianHomeVal)
+        session.add(new)
     
 session.commit()
 session.flush()
